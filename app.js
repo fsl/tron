d7c7de63840b626de37ee3dc20c06c2c238765ec
Stephan Hoyer <ste.hoyer@gmail.com>
2012-12-21 11:09:51 +0100

    Spieler zeichnen

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var context;
-var spielfeldbreite = 100;
-var spielfeldhoehe = 100;
+var spielfeldbreite = 50;
+var spielfeldhoehe = 50;
+var spieler1;
+var spieler2;
 var raster = 10;
 
 function erstelleSpielfeld(breite, hoehe) {
@@ -14,19 +16,54 @@ function erstelleSpielfeld(breite, hoehe) {
   return spielfeld;
 }
 
-function kreis(x, y, radius, farbe) {
+function kreis(x, y, radius, farbe, context) {
   context.beginPath();
   context.arc(x, y, radius, 0, 2 * Math.PI, false);
   context.fillStyle = farbe;
   context.fill();
 }
 
-function zeichneFeld(spielfeld, context) {
-  for(var x = 0; x<spielfeld.length; x++) {
-    for(var y = 0; y<spielfeld[x].length; y++) {
-      kreis(x*raster, y*raster, 1, 'red'); 
+function rechteck(x, y, breite, hoehe, farbe, context) {
+  context.beginPath();
+  context.rect(x, y, breite, hoehe);
+  context.fillStyle = farbe;
+  context.fill();
+}
+
+function zeichneFeld(spielfeld, spielers, context) {
+  for (var x = 0; x<spielfeld.length; x++) {
+    for (var y = 0; y<spielfeld[x].length; y++) {
+      kreis(x*raster, y*raster, 1, 'white', context); 
     }
   }
+
+  spielers.forEach(function (spieler) {
+    rechteck(
+      raster * (spieler.x-1/2),
+      raster * (spieler.y-1/2), 
+      raster, 
+      raster, 
+      spieler.farbe, 
+      context
+    );
+  });
+}
+
+function erstelleSpieler(breite, hoehe)
+{
+  return [
+  {
+    x: 10,
+    y: hoehe/2,
+    richtung: 3,
+    farbe: 'blue'
+  }, {
+    x: breite-10,
+    y: hoehe/2,
+    richtung: 9,
+    farbe: 'purple'
+  }
+  ]
 }
 
 window.onload = function(){
@@ -34,6 +71,8 @@ window.onload = function(){
   context = canvas.getContext("2d");
   context.font = "normal 40px Courier New";
   var spielfeld = erstelleSpielfeld(spielfeldbreite, spielfeldhoehe);
-  zeichneFeld(spielfeld, context);
+  var spieler = erstelleSpieler(spielfeldbreite, spielfeldhoehe);
+  zeichneFeld(spielfeld, spieler, context);
+
 };
 
